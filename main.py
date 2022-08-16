c87f1670989fc6bd4ee465c32c66cd49f04ed274
Kamil Foltynski <kamil@foltynski.com>
2022-08-16 17:57:57 +0200

    clenup in main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 from funs import *
-import numpy as np
-
-
-# board = np.array([['b', 'r', 'r', 'b', 'r'],
-#        ['y', 'r', 'b', 'r', 'b'],
-#        ['b', 'r', 'g', 'g', 'y'],
-#        ['b', 'r', 'g', 'b', 'y'],
-#        ['r', 'r', 'b', 'b', 'b']], dtype='<U1')
-# start_game(board)
-
 
 start_game()
